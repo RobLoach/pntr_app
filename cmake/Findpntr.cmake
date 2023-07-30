8c39d1544ac4cb5e371d51017949800725e98d9e
Rob Loach <robloach@gmail.com>
2023-07-30 19:08:02 -0400

    Add ghpages

--- cmake/Findpntr.cmake
+++
@@ -1,8 +0,0 @@
-# pntr
-include(FetchContent)
-FetchContent_Declare(
-    pntr
-    GIT_REPOSITORY https://github.com/robloach/pntr.git
-    GIT_TAG be5758bc69e34200ff7f880cdeccb600fcaac639
-)
-FetchContent_MakeAvailable(pntr)
