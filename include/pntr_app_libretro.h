6201b4157c0b677ab6d0b69774d3475c5ae55585
Rob Loach <robloach@gmail.com>
2023-10-05 14:35:26 -0400

    libretro: Move the VFS to its own file

--- include/pntr_app_libretro.h
+++ include/pntr_app_libretro.h
@@ -25,88 +25,11 @@ pntr_app* pntr_app_libretro;
 
 static struct retro_log_callback logging;
 static retro_log_printf_t log_cb;
+
+// File System
 struct retro_vfs_interface* vfs;
-
-#ifndef PNTR_LOAD_FILE
-    /**
-     * Load a file using libretro's virtual file system.
-     */
-    unsigned char* pntr_app_libretro_load_file(const char* fileName, unsigned int* bytesRead) {
-        if (vfs == NULL) {
-            return NULL;
-        }
-
-        // Open the file
-        struct retro_vfs_file_handle* file = vfs->open(fileName, RETRO_VFS_FILE_ACCESS_READ, RETRO_VFS_FILE_ACCESS_HINT_NONE);
-        if (file == NULL) {
-            if (bytesRead != NULL) {
-                *bytesRead = 0;
-            }
-            return NULL;
-        }
-
-        // Get the size
-        int64_t size = vfs->size(file);
-        if (size <= 0) {
-            vfs->close(file);
-            if (bytesRead != NULL) {
-                *bytesRead = 0;
-            }
-            return NULL;
-        }
-
-        // Prepare the data
-        unsigned char* data = (unsigned char*)pntr_load_memory(size * sizeof(unsigned char) + 1);
-        if (data == NULL) {
-            vfs->close(file);
-            if (bytesRead != NULL) {
-                *bytesRead = 0;
-            }
-            return NULL;
-        }
-
-        // Read the file
-        unsigned int bytes = (unsigned int)vfs->read(file, data, size);
-        vfs->close(file);
-        if (bytesRead != NULL) {
-            *bytesRead = bytes;
-        }
-
-        return data;
-    }
-    #define PNTR_LOAD_FILE pntr_app_libretro_load_file
-#endif
-
-#ifndef PNTR_SAVE_FILE
-    /**
-     * Save a file using libretro's file system.
-     */
-    bool pntr_app_libretro_save_file(const char *fileName, const void *data, unsigned int bytesToWrite) {
-        if (vfs == NULL) {
-            return false;
-        }
-
-        // Open the file
-        struct retro_vfs_file_handle* file = vfs->open(fileName, RETRO_VFS_FILE_ACCESS_WRITE, RETRO_VFS_FILE_ACCESS_HINT_NONE);
-        if (file == NULL) {
-            return false;
-        }
-
-        // Write the file
-        int64_t bytesWritten = vfs->write(file, data, (uint64_t)bytesToWrite);
-        if (bytesWritten <= 0) {
-            vfs->close(file);
-            return false;
-        }
-
-        // Flush the buffer and close the file.
-        vfs->flush(file);
-        vfs->close(file);
-
-        return true;
-    }
-    #define PNTR_SAVE_FILE(fileName, data, bytesToWrite) pntr_app_libretro_save_file(fileName, data, bytesToWrite)
-#endif
+#define PNTR_APP_LIBRETRO_VFS vfs
+#include "extensions/pntr_app_libretro_vfs.h"
 
 typedef struct pntr_app_libretro_platform {
     int16_t mouseButtonState[PNTR_APP_MOUSE_BUTTON_LAST];
@@ -1024,7 +947,7 @@ pntr_sound* pntr_load_sound_from_memory(pntr_app_sound_type type, unsigned char*
     }
 
     if (sound == NULL) {
-        log_cb(RETRO_LOG_INFO, "[pntr] Failed to load audio data from %s\n", fileName);
+        log_cb(RETRO_LOG_INFO, "[pntr] Failed to load audio data\n");
         pntr_unload_file(data);
         return NULL;
     }
