740718ed46c4cd91cab4ba661edaebe7c749c0dc
Rob Loach <robloach@gmail.com>
2023-12-18 12:15:36 -0500

    Update pntr

--- cmake/Findpntr.cmake
+++ cmake/Findpntr.cmake
@@ -3,6 +3,6 @@ include(FetchContent)
 FetchContent_Declare(
     pntr
     GIT_REPOSITORY https://github.com/robloach/pntr.git
-    GIT_TAG 4e373dde963cd1a4e741dc1df6619ceadf94b9a6
+    GIT_TAG 6db29775523a7c8d381b2b4a20fad75b551e9fda
 )
 FetchContent_MakeAvailable(pntr)
