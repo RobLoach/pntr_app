5b35f1a9c276c3cc05b7148b4a1a350661f359ab
Rob Loach <robloach@gmail.com>
2026-01-24 22:20:36 -0500

    Update external

--- cmake/FindSDL3.cmake
+++ cmake/FindSDL3.cmake
@@ -2,7 +2,7 @@ include(FetchContent)
 FetchContent_Declare(
     SDL3Source
     GIT_REPOSITORY https://github.com/libsdl-org/SDL.git
-    GIT_TAG 2fa1e72
+    GIT_TAG a962f40bbba175e9716557a25d5d7965f134a3d3
     #GIT_SHALLOW 1
 )
 FetchContent_MakeAvailable(SDL3Source)
