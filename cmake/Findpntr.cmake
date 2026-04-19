3558774b8f8f8b2e8aa7e14e531ab4d56d5286ef
Rob Loach <robloach@gmail.com>
2026-04-19 12:48:05 -0400

    Fix cmake warning

--- cmake/Findpntr.cmake
+++ cmake/Findpntr.cmake
@@ -3,5 +3,6 @@ include(FetchContent)
 FetchContent_Declare(
     pntr
     URL https://github.com/RobLoach/pntr/archive/refs/heads/master.zip
+    DOWNLOAD_EXTRACT_TIMESTAMP TRUE
 )
 FetchContent_MakeAvailable(pntr)
